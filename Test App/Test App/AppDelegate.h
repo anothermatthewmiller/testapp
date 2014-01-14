eb9d7188fa993243feef7d74676a62a858e62dcd
anothermatthewmiller <mmiller.cs03@gtalumni.org>
2014-01-14 18:44:04 -0500

    Initial Commit

---
+++ Test App/Test App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test App
+//
+//  Created by Matthew Miller on 1/14/14.
+//  Copyright (c) 2014 Matthew Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test App/Test App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test App
+//
+//  Created by Matthew Miller on 1/14/14.
+//  Copyright (c) 2014 Matthew Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
